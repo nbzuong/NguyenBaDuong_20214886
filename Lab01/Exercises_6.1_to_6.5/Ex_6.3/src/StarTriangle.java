67bd3e1d645f41483bbc0945d3800e0fdb30549d
nbzuong <dnb.020703@gmail.com>
2023-04-15 22:37:32 +0700

    done 6.3

--- Lab01/Exercises_6.1_to_6.5/Ex_6.3/src/StarTriangle.java
+++ Lab01/Exercises_6.1_to_6.5/Ex_6.3/src/StarTriangle.java
@@ -6,14 +6,14 @@ public class StarTriangle {
 		Scanner height = new Scanner(System.in);
 		
 		//Get input
-		System.out.print("Enter the height of the triange n= ");
+		System.out.print("Enter the height of the triangle n= ");
 		int n = height.nextInt();
 		if (n<=0) {
 			System.out.print("The height must larger than 0");
 		}
 		else {
 			for (int i = 0; i < n; i++) {
-				for (int j = 0; j <= n-i; j++) {
+				for (int j = 1; j < n-i; j++) {
 					System.out.print(" ");
 				}
 				for (int k = 1; k <= 2*i+1; k++) {
